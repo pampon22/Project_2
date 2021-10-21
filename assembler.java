c3fa2ac837edf459c3141866bfdc4a007ff495e4
Philip Ampong <58187425+pampon22@users.noreply.github.com>
2021-10-21 02:37:11 -0400

    Update assembler.java

--- assembler.java
+++ assembler.java
@@ -1,3 +1,3 @@
-write tests for peak, pull, push, pop on empty list, single sized list, n>1 list
+// write tests for peak, pull, push, pop on empty list, single sized list, n>1 list
 
-methods: isempty(), peek(), push(sth), pop(), size(), 
+// methods: isempty(), peek(), push(sth), pop(), size(), 
